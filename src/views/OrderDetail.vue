faeca1453b95990d06676f3ec104860c6ba0c1b3
刘家亮 <ljl@nssp.biz>
2018-09-05 16:59:17 +0800

    完善el-dialog组件，支持data数据缓存

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -37,8 +37,81 @@
             </el-table>
         </el-col>
         <!-- 查看详细 -->
-        <el-dialog title="订单详细" :visible.sync="isShowDetail" center width="30%">
-            <div>混子</div>
+        <el-dialog title="订单详细" :visible.sync="isShowDetail" center width="800px">
+            <el-row>
+              <el-col :span="24">
+                  <el-button type="primary" size="small" style="margin-bottom:10px">用户信息</el-button>
+                  <table class="table table-user">
+                      <tbody>
+                          <tr>
+                              <td>用户ID</td>
+                              <td>{{detailForm.memberID}}</td>
+                              <td>用户手机</td>
+                              <td>{{detailForm.memberTel}}</td>
+                          </tr>
+                          <tr>
+                              <td>用户账号</td>
+                              <td>{{detailForm.memberAccount}}</td>
+                              <td></td>
+                              <td></td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </el-col>
+              <el-col :span="24" style="margin-top:10px">
+                  <el-button type="info" size="small" style="margin-bottom:10px">订单信息</el-button>
+                  <table class="table table-order">
+                      <tbody>
+                          <tr>
+                              <td>订单号</td>
+                              <td>{{detailForm.orderId}}</td>
+                              <td>订单状态</td>
+                              <td>{{detailForm.status}}</td>
+                          </tr>
+                          <tr>
+                              <td>支付方式</td>
+                              <td>{{detailForm.paymentType}}</td>
+                              <td>支付金额</td>
+                              <td>{{detailForm.totalPayment}}</td>
+                          </tr>
+                          <tr>
+                              <td>下单时间</td>
+                              <td>{{detailForm.orderTime}}</td>
+                              <td>支付时间</td>
+                              <td>{{detailForm.orderTime}}</td>
+                          </tr>
+                          <tr>
+                              <td class="red">申请退款时间</td>
+                              <td class="red">{{detailForm.refundTime}}</td>
+                              <td class="red">退款金额</td>
+                              <td class="red">{{detailForm.totalPayment}}</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </el-col>
+              <el-col :span="24" style="margin-top:10px">
+                  <el-button type="warning" size="small" style="margin-bottom:10px">商品信息</el-button>
+                  <table class="table table-user">
+                      <tbody>
+                          <tr>
+                              <td>套餐类型</td>
+                              <td>{{detailForm.packageType}}</td>
+                              <td>到期时间</td>
+                              <td>{{detailForm.packageEndTime}}</td>
+                          </tr>
+                          <tr>
+                              <td>开始时间</td>
+                              <td>{{detailForm.packageStartTime}}</td>
+                              <td></td>
+                              <td></td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </el-col>
+            </el-row>
+            <span slot="footer" class="dialog-footer" style="text-align:right">
+                <el-button type="primary" @click="isShowDetail = false">确定</el-button>
+            </span>
         </el-dialog>
     </div>
 </template>
@@ -51,14 +124,39 @@ export default {
                 value: '全部',
                 payment: '全部',
             },
+            detailForm: {
+                memberID: "",
+                memberTel: "",
+                memberAccount: "",
+                orderId: "",
+                status: "",
+                memberID: "",
+                memberAccount: "",
+                totalPayment: "",
+                paymentType: "",
+                orderTime: "",
+                payTime: "",
+                packageType: "",
+                packageStartTime: "",
+                packageEndTime: "",
+                refundTime: ""
+            },
             tableData: [{
+                memberID: "1212131313",
+                memberTel: "13265699624",
+                memberAccount: "13265699624",
                 orderId: "454545454",
                 status: "待付款",
                 memberID: "1212131313",
                 memberAccount: "133111111",
                 totalPayment: "¥30",
                 paymentType: "微信",
-                orderTime: "2018.08.23  12:20:20"
+                orderTime: "2018.08.23  12:20:20",
+                payTime: "2018.08.23  12:20:20",
+                packageType: "黄金会员月度卡",
+                packageStartTime: "2018.08.23  12:20:20",
+                packageEndTime: "2018.08.26  12:20:20",
+                refundTime: "2018.08.26  12:20:20"
             }],
             isShowDetail: false,
             payments: [{
@@ -90,9 +188,32 @@ export default {
     methods: {
         handleEdit(row) {
             this.isShowDetail = true;
+            this.detailForm = Object.assign({},row,true);
+            console.log(this.detailForm);
         }
     }
 }
 </script>
+<style lang="scss">
+.table{
+    position: relative;
+    width: 100%;
+    border-collapse: collapse;
+    margin: 0 auto;
+    empty-cells: show;
+    td {
+        border: 1px #eee solid;
+        width: 25%;
+        height: 30px;
+        line-height: 30px;
+        overflow: hidden;
+        text-align: center;
+        &.red {
+            color: red;
+        }
+    }
+}
+</style>
+
 
 
